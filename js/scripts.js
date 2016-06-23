f42065fa7d08bf425c41f5aa630551d34608ec6f
jgoldsmith83 <mapexmelee@gmail.com>
2016-06-23 13:21:43 -0400

    made the carousel full width - not sure if I like it. Added hide and show effect to the left and right carousel control on hover and mouseleave.

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+  //Hide the left and right controls on the carousel when the page loads
+  $('.carousel-control').hide();
+
+  //Sets automatic sliding at a 5 second interval
+  $('.carousel').carousel({
+     interval: 5000
+  });
+
+  //Show the left and right controls while the mouse is over the carousel
+  $('.carousel').hover(function() {
+    $('.carousel-control').fadeIn('fast');
+  });
+
+  //Hide the left and right controls again after the mouse moves out of the carousel
+  $('.carousel').mouseleave(function() {
+    $('.carousel-control').fadeOut('fast');
+  });
+
+});
