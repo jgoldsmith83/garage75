ca9c6052163405f9105efbd064b754ebf55c4253
jgoldsmith83 <mapexmelee@gmail.com>
2016-06-23 22:36:03 -0400

    Card styling on menu starting to come together.

--- js/menu.js
+++ js/menu.js
@@ -4,8 +4,13 @@ $(document).ready(function() {
   //$('.menu-item:even').css({'background-color':'#f9f9f9'});
   //$('.menu-item:even').css({'color':'#585959'});
 
-  $('.menu-item:even').css({'border-top':'1px solid #f9f9f9'});
-  $('.menu-item:even').css({'border-bottom':'1px solid #f9f9f9'});
+  /* Using jquery to add styling to make it eaiser to interact with items generated from JSON */
+
+  //set top and bottom border on each menu item with an even $index for a hint of item separation
+  //$('.menu-item:even').css({'border-top':'1px solid #f9f9f9'});
+  //$('.menu-item:even').css({'border-bottom':'1px solid #f9f9f9'});
+
+  //add some padding to each menu item so they don't all run together
   $('.menu-item').css({'padding':'10px 10px 10px 10px'});
 
 })
